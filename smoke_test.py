22bef426e1b060ab94e4e830d9ef917670ed9b6b
divaychauhan <divaypratap5113@gmail.com>
2026-07-06 10:29:20 +0530

    newfile

--- smoke_test.py
+++ smoke_test.py
@@ -29,6 +29,7 @@ try:
         host=params["DB_HOST"],
         user=params["DB_USER"],
         password=params["DB_PASSWORD"],
+        database=params["DB_NAME"],
         port=int(params["DB_PORT"]),
         connect_timeout=10
     )
